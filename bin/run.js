3789689a4b97d05b6a757e2177b551a72ece1cf6
lia setiawan <liasetiawan@gmail.com>
2018-06-02 10:47:10 +0700

    masih error

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,18 @@
 'use strict';
 
+const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
-
 const server = http.createServer(service);
-server.listen(3000);
+const slackToken = 'cnH3o7WQBKONc54QobPGhLte';
+const slackLogLevel='debug';
+
+const rtm = slackClient.init(slackToken, slackLogLevel);
+rtm.start();
+
+slackClient.addAuthenticatedHandler(rtm,()=> server.listen(3000));
 
 server.on('listening', function(){
 console.log(`helloslack is listening on ${server.address().port} in ${service.get('env')}`);
+
 });
--- server/slackClient.js
+++ server/slackClient.js
@@ -1,20 +1,19 @@
-t { RTMClient } = require('@slack/client');
+'use strict';
 
-// An access token (from your Slack app or custom integration - usually xoxb)
- const token = process.env.SLACK_TOKEN;
-//
-// // The client is initialized and then started to get an active connection to the platform
-// const rtm = new RTMClient(token);
- rtm.start();
-//
-// // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-// // See the "Combining with the WebClient" topic below for an example of how to get this ID
- const conversationId = 'C1232456';
-//
-// // The RTM client can send simple string messages
- rtm.sendMessage('Hello there', conversationId)
-   .then((res) => {
-      // `res` contains information about the posted message
-           console.log('Message sent: ', res.ts);
-             })
-               .catch(console.error);
+const RtmClient  = require('@slack/client').RTMClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+function handleOnAuthenticated(rtmStartData){
+	console.log(`Logged in as ${rtmStartData.sel.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+
+}
+
+function addAuthenticatedHandler(rtm, handler){
+	rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED,handler);
+}
+
+module.exports.init= function slackClient(token, logLevel){
+	const rtm = new RtmClient(token, {logLevel: 'debug'});
+	addAuthenticatedHandler(rtm, handleOnAuthenticated);
+	return rtm;
+}
